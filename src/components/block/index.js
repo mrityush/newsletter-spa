ca347f499e68891c670b833daab2914eec3994ee
mj <mrityunjay.shukla@gmail.com>
2021-02-02 08:33:24 +0530

    test changes. Need to confirm with vineet or vaibhav

---
+++ src/components/block/index.js
@@ -0,0 +1,13 @@
+import "./block.css";
+
+const BlockBox = () => {
+  return (
+    <div>
+      <div id="blockBox" className="red-block">
+        Is it working ?
+      </div>
+    </div>
+  );
+};
+
+export default BlockBox;
---
+++ src/components/navbar/index.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <div>
+      <div id="navbarBlock">
+        <ul className="navbar-nav">THE UPSKILL NETWORK</ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
